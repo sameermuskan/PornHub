8e385c0523e445669f8b754da1382292d0b01663
sameermuskan <61672655+sameermuskan@users.noreply.github.com>
2020-03-03 22:50:24 +0530

    Update autopic.py

--- stdplugins/autopic.py
+++ stdplugins/autopic.py
@@ -33,7 +33,7 @@ async def autopic(event):
     photo = "photo_pfp.png"
     while not downloader.isFinished():
         place_holder = None
-    counter = -10
+    counter = -30
     while True:
         shutil.copy(downloaded_file_name, photo)
         im = Image.open(photo)
@@ -41,8 +41,8 @@ async def autopic(event):
         current_time = datetime.now().strftime("🔘 \n S❤️M \n  Time: %H:%M:%S \n  Date: %d.%m.%y \n 🔘")
         img = Image.open(photo)
         drawn_text = ImageDraw.Draw(img)
-        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 20)
-        drawn_text.text((200, 200), current_time, font=fnt, fill=(0, 0, 0))
+        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
+        drawn_text.text((250, 250), current_time, font=fnt, fill=(255, 255, 255))
         img.save(photo)
         file = await event.client.upload_file(photo)  # pylint:disable=E0602
         try:
@@ -50,7 +50,7 @@ async def autopic(event):
                 file
             ))
             os.remove(photo)
-            counter -= 10
-            await asyncio.sleep(20)
+            counter -= 30
+            await asyncio.sleep(30)
         except:
             return
