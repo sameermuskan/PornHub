421d6836d5456bf7fc548c33c842decbe2b65283
sameermuskan <61672655+sameermuskan@users.noreply.github.com>
2020-03-07 07:06:43 +0530

    Update karbon1.py

--- stdplugins/karbon1.py
+++ stdplugins/karbon1.py
@@ -62,7 +62,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         c
+         caption="Karbon1 by [@Sameer](https://t.me/ILoveYouMuskan)",
          force_document=False,
          reply_to=e.message.reply_to_msg_id,
          )
