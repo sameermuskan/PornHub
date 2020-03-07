3ac81e042f5e29cccaa18a7874bfddc4fb2711f7
sameermuskan <61672655+sameermuskan@users.noreply.github.com>
2020-03-07 20:07:55 +0530

    Update autopic.py

--- stdplugins/autopic.py
+++ stdplugins/autopic.py
@@ -41,7 +41,7 @@ async def autopic(event):
         current_time = datetime.now().strftime("🌜 \n S❤️M \n  Time: %H:%M:%S \n  Date: %d.%m.%y \n 🌜")
         img = Image.open(photo)
         drawn_text = ImageDraw.Draw(img)
-        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
+        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 23)
         drawn_text.text((250, 250), current_time, font=fnt, fill=(0, 0, 0))
         img.save(photo)
         file = await event.client.upload_file(photo)  # pylint:disable=E0602
@@ -51,6 +51,6 @@ async def autopic(event):
             ))
             os.remove(photo)
             counter -= 30
-            await asyncio.sleep(30)
+            await asyncio.sleep(20)
         except:
             return
